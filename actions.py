7c1c3ecaa22314e258cc75d2cf571892f939d7ba
kjg <kgawda@gmail.com>
2017-03-13 22:08:33 +0100

    small correction

--- actions.py
+++ actions.py
@@ -9,6 +9,6 @@ def action1(user, password, blogURL, message):
                 "text": "<p>This is <b>scripted post</b>.<p>%s<p>Sent at %s" % (message, datetime.datetime.now())
             }}
 
-    r = requests.post(blogURL, auth=('user', 'password'), json=data)
+    r = requests.post(blogURL, auth=(user, password), json=data)
 
     return r.status_code, r.text[:100]
