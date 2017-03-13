7c09efe66a46ddd921bd86369f5132e8763e7f88
Konrad Gawda <konrad.gawda@orange.com>
2017-03-13 18:38:29 +0100

    stub pages

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,19 @@
+import os
+
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@application.route("/test")
+def testpage():
+    return "Test passed for "+os.environ.get('APP_MAIN_USER','user')
+
+@application.route("/post")
+def post():
+    return "TBD"
 
 if __name__ == "__main__":
     application.run()
