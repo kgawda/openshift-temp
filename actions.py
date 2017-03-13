1ef35461e2b488ddaad951f2d5de934e839405ac
kjg <kgawda@gmail.com>
2017-03-13 20:57:17 +0100

    posting

---
+++ actions.py
@@ -0,0 +1,14 @@
+import requests, datetime
+
+def action1(user, password, blogURL, message):
+
+    data = {
+            "subject": "Test post...", "type": "post",
+            "content": {
+                "type": "text/html",
+                "text": "<p>This is <b>scripted post</b>.<p>%s<p>Sent at %s" % (message, datetime.datetime.now())
+            }}
+
+    r = requests.post(blogURL, auth=('user', 'password'), json=data)
+
+    return r.status_code, r.text[:100]
--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,63 @@
 import os
 
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask, session, redirect, url_for, escape, request, abort
+import actions
+app = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/")
+def index():
+    return "Hello"
 
-@application.route("/test")
+@app.route("/test")
 def testpage():
-    return "Test passed for "+os.environ.get('APP_MAIN_USER','user')
+    return "Test for %s, logged-%s." % (
+        escape(os.environ.get('APP_MAIN_USER','user')),
+        'in' if 'password' in session else 'out'
+        )
 
-@application.route("/post")
+@app.route("/post", methods=['GET', 'POST'])
 def post():
-    return "TBD"
+    if not 'password' in session:
+        abort(401)
+    if request.method == 'POST':
+        message = request.form.get('message', '')
+        user = os.environ['APP_MAIN_USER']
+        password = session['password']
+        blogURL = os.environ['APP_BLOG_URL']
+
+        print actions.action1(user, password, blogURL, message)
+
+        return redirect(url_for('post'))
+
+    return '''
+        <form method="post">
+            <textarea name="message" rows="3" cols="40"></textarea>
+            <p><input type=submit value=Send>
+        </form>
+    '''
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'].strip() == os.environ['APP_MAIN_USER']:
+            session['password'] = request.form['password']
+            return redirect(url_for('index'))
+        else:
+            abort(401)
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    session.pop('password', None)
+    return redirect(url_for('index'))
+
+app.secret_key = os.environ['APP_SECRET_KEY']
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
